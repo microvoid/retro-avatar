9a246c0cc665a0fa6efb531a53349c727384ae22
whistleyz <whistleyz@163.com>
2023-03-24 16:06:47 +0800

    fix: validate id input

--- apps/website/utils/avatars.ts
+++ apps/website/utils/avatars.ts
@@ -3,6 +3,10 @@ import { theme } from 'retro-avatar'
 const themes = Object.keys(theme) as (keyof typeof theme)[]
 
 export function genAvatars(id: string) {
+  if (!id) {
+    return []
+  }
+
   return themes.map((t) => {
     return {
       url: `/api/avatar/${id}?t=${t}`,
